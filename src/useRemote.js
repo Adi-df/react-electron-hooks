320e9f7b10ff69fb48c51e1a25cf560063ca14ee
Adi-df <adidf-web@laposte.net>
2021-02-20 16:35:57 +0100

    Create useRemote hook

---
+++ src/useRemote.js
@@ -0,0 +1,13 @@
+export default function useRemote() {
+  if (/electron\/(\d+\.\d+\.\d+)/i.test(navigator.userAgent)) {
+    let electron = window.require("electron");
+    if (!electron.remote) {
+      throw new Error("Be sure that enableRemoteMenu is to true");
+    }
+    return electron.remote;
+  } else {
+    throw new Error(
+      "Not inside a electron process, please use useElectron to ensure that!"
+    );
+  }
+}
