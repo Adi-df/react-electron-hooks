3ad2f5172c717e9ae19721edcc13e57beb200a8f
Adi-df <adidf-web@laposte.net>
2021-02-21 17:29:11 +0100

    Add useIpc hook

---
+++ src/useIpc.js
@@ -0,0 +1,13 @@
+export default function useIpc() {
+  if (/electron\/(\d+\.\d+\.\d+)/i.test(navigator.userAgent)) {
+    let electron = window.require("electron");
+    if (!electron.ipcRenderer) {
+      throw new Error("Be sure that enableRemoteMenu is to true");
+    }
+    return electron.ipcRenderer;
+  } else {
+    throw new Error(
+      "Not inside a electron process, please use useElectron to ensure that!"
+    );
+  }
+}
