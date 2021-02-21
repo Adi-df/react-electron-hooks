0d3dca891ccba380b60cafdca6dd7b84c74a9659
Adi-df <adidf-web@laposte.net>
2021-02-21 17:34:43 +0100

    Test useIpc hook

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 function createWindow() {
   // Create the browser window.
@@ -25,6 +25,9 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
+
+// Test ipcMessages
+ipcMain.handle("ping", (e, n) => Promise.resolve(`pong ${n * 2}`));
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import useElectron from "./useElectron";
+import useIpc from "./useIpc";
 import useRemote from "./useRemote";
 
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  useElectron(
-    v => {
-      console.log(`There is Electron version ${v}`);
-      let { BrowserWindow } = useRemote();
-
-      let win = new BrowserWindow();
-      win.loadURL("https://github.com");
-      win.show();
-    },
-    () => console.log("There isn't Electron")
-  );
-
   return (
     <div className="App">
       <header className="App-header">
@@ -27,11 +16,49 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
+          href="https://github.com/"
           rel="noopener noreferrer"
+          onClick={e => {
+            useElectron(
+              v => {
+                e.preventDefault();
+                console.log(`There is Electron version ${v}`);
+
+                let { BrowserWindow } = useRemote();
+
+                let win = new BrowserWindow();
+                win.loadURL("https://github.com");
+                win.show();
+              },
+              () => console.log("There isn't Electron")
+            );
+          }}
         >
-          Learn React
+          Open github
+        </a>
+        <a
+          className="App-link"
+          target="_blank"
+          href="https://www.techwalla.com/articles/how-to-ping-a-server-name"
+          rel="noopener noreferrer"
+          onClick={e => {
+            e.preventDefault();
+            useElectron(
+              v => {
+                console.log(`There is Electron version ${v}`);
+
+                let { invoke } = useIpc();
+                invoke("ping", 3).then(res => console.log(res));
+              },
+              () => {
+                console.log("There isn't Electron")
+                console.log(`pong ${3/2}`)
+              }
+            );
+          }}
+        >
+          Run ping
         </a>
       </header>
     </div>
